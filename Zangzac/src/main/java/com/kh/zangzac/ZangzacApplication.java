69616f17e20d9b603f9852bfb7dd4f5b235ed28e
CHOI <CHOI@112.157.44.13>
2023-12-17 11:39:17 +0900

    testPage2

---
+++ Zangzac/src/main/java/com/kh/zangzac/ZangzacApplication.java
@@ -0,0 +1,13 @@
+package com.kh.zangzac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZangzacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZangzacApplication.class, args);
+	}
+
+}
